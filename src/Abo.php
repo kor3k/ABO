976a98d313b644da68301646ee1552c51cf5058e
Ciki <matula.m@gmail.com>
2021-01-26 10:48:24 +0100

    Abo: add phpDoc, reorder methods, always generate security codes, remove getFiles()

--- src/Abo.php
+++ src/Abo.php
@@ -9,15 +9,26 @@ use snoblucha\Abo\Account\File;
 
 class Abo
 {
+	/** @type string 4 chars */
 	const HEADER = 'UHL1';
 
 
 	/** @var File[] */
 	private array $items = [];
+
+	/** @var ?string 20 chars */
 	private ?string $organization = null;
+
+	/** @var ?string format ddmmyy */
 	private ?string $date = null;
+
+	/** @var ?string 10 numbers */
 	private ?string $clientNumber = null;
+
+	/** @var ?string 6 numbers */
 	private ?string $securityCodeFixedPart = null;
+
+	/** @var ?string 6 numbers */
 	private ?string $securityCodeSecretPart = null;
 
 
@@ -46,22 +57,6 @@ class Abo
 	}
 
 
-	/**
-	 * Optional part of the header. Set the Fixed key part and security code.
-	 * @param string $fixed 6 numbers
-	 * @param string $secret 6 numbers
-	 */
-	public function setSecurityKey(string $fixed, string $secret): self
-	{
-		if (!is_numeric($fixed) || !is_numeric($secret) || strlen($fixed) !== 6 || strlen($secret) !== 6) {
-			throw new InvalidArgumentException('Parameters $fixed and $secret must be numeric strings of length 6!');
-		}
-		$this->securityCodeFixedPart = $fixed;
-		$this->securityCodeSecretPart = $secret;
-		return $this;
-	}
-
-
 	public function setDate(?DateTimeInterface $date = null): self
 	{
 		if ($date == null) {
@@ -83,7 +78,22 @@ class Abo
 	}
 
 
-	public function addAccountFile($type = File::TYPE_UHRADA): File
+	/**
+	 * @param string $fixed 6 numbers
+	 * @param string $secret 6 numbers
+	 */
+	public function setSecurityKey(string $fixed, string $secret): self
+	{
+		if (!is_numeric($fixed) || !is_numeric($secret) || strlen($fixed) !== 6 || strlen($secret) !== 6) {
+			throw new InvalidArgumentException('Parameters $fixed and $secret must be numeric strings of length 6!');
+		}
+		$this->securityCodeFixedPart = $fixed;
+		$this->securityCodeSecretPart = $secret;
+		return $this;
+	}
+
+
+	public function addAccountFile(int $type = File::TYPE_UHRADA): File
 	{
 		$item = new File($type);
 		$this->items[] = $item;
@@ -92,25 +102,14 @@ class Abo
 	}
 
 
-	/**
-	 * Get the account files
-	 * @return File[]
-	 */
-	public function getFiles(): array
-	{
-		return $this->items;
-	}
-
-
 	public function generate(): string
 	{
 		$res = sprintf("%4s%6s%-20s%010d%03d%03d", self::HEADER, $this->date, $this->organization, $this->clientNumber, 1, 1 + count($this->items));
-		if ($this->securityCodeSecretPart) {
-			$res .= sprintf("%06d%06d", $this->securityCodeFixedPart, $this->securityCodeSecretPart);
-		}
+		$res .= sprintf("%06d%06d", $this->securityCodeFixedPart, $this->securityCodeSecretPart);
 		$res .= "\r\n";
 
 		foreach ($this->items as $item) {
+			/** @var File $item */
 			$res .= $item->generate();
 		}
 
